b75a3d1f7e297a04c8b300c6683602a28beae815
Dawid Urbaniak <dawid.urbaniak@dataart.com>
2018-03-11 12:36:12 +0100

    Removed not needed files.

--- gui/src/main/java/Main.java
+++ gui/src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
